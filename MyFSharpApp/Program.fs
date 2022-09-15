524d3be3caced68555f1a72aa2752e29bb64766f
David Martel <daveymartel@gmail.com>
2022-09-15 19:59:04 +0000

    hello world

---
+++ MyFSharpApp/Program.fs
@@ -0,0 +1,16 @@
+﻿// // Learn more about F# at http://fsharp.org
+
+open System
+
+// [<EntryPoint>]
+// let main argv =
+//     printfn "Hello World from F#!"
+//     0 // return an integer exit code
+
+
+// Define a new function to print a name.
+let printGreeting =
+    printfn "Hello %s from F#!"
+
+// Call your new function!
+do printGreeting "Dave"
